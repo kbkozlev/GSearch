4cb4084514cb76bbc29e6a7e69fea096332cbcd8
Kaloian Kozlev <kbkozlev@gmail.com>
2024-01-17 14:10:58 +0200

    Add files via upload

--- googleSearch/background.js
+++ googleSearch/background.js
@@ -1,4 +1,21 @@
+// background.js
 chrome.runtime.onInstalled.addListener(function () {
+
+  chrome.contextMenus.create({
+    id: "removeExtension",
+    title: "Remove Extension",
+    contexts: ["browser_action"],
+    parentId: "extensionContextMenu"
+  });
+
+  chrome.contextMenus.create({
+    id: "openGitHub",
+    title: "Open GitHub Page",
+    contexts: ["browser_action"],
+    parentId: "extensionContextMenu"
+  });
+
+  // Context menu for right-click on web pages
   chrome.contextMenus.create({
     id: "googleSearch",
     title: "Search with Google",
@@ -7,7 +24,11 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (info.menuItemId === "googleSearch" && info.selectionText) {
+  if (info.menuItemId === "removeExtension") {
+    chrome.management.uninstallSelf({ showConfirmDialog: true });
+  } else if (info.menuItemId === "openGitHub") {
+    chrome.tabs.create({ url: "https://github.com/kbkozlev/googleSearch" });
+  } else if (info.menuItemId === "googleSearch" && info.selectionText) {
     searchWithGoogle(info.selectionText);
   }
 });
