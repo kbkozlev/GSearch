7af185074a3c8794f1dead306e8c943ebbedde90
kbkoz <kbkozlev@gmail.com>
2024-01-29 12:03:27 +0200

    renamed main folder

---
+++ GSearch/background.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener(function () {
+  createContextMenu("GSearch", "Search with Google", ["selection"]);
+});
+
+chrome.contextMenus.onClicked.addListener(function (info) {
+  if (info.menuItemId === "GSearch" && info.selectionText) {
+    searchWithGoogle(info.selectionText);
+  }
+});
+
+function createContextMenu(id, title, contexts) {
+  chrome.contextMenus.create({
+    id: id,
+    title: title,
+    contexts: contexts,
+  });
+}
+
+function searchWithGoogle(query) {
+  const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+
+  chrome.tabs.create({ url: searchUrl }, function() {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+    }
+  });
+}
